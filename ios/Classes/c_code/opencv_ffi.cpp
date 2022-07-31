bc3e61ba39caf1ef87e2dd09086ca3125748ac25
Alexander Mayorga <alexmayorga@knights.ucf.edu>
2022-07-31 17:03:48 -0400

    Initial Commit

---
+++ ios/Classes/c_code/opencv_ffi.cpp
@@ -0,0 +1,13 @@
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+// Avoiding name mangling
+extern "C" {
+    // Attributes to prevent 'unused' function from being removed and to make it visible
+    __attribute__((visibility("default"))) __attribute__((used))
+    const char* version() {
+        return CV_VERSION;
+    }
+}
---
+++ lib/opencv_ffi.dart
@@ -0,0 +1,13 @@
+
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class OpencvFfi {
+  static const MethodChannel _channel = MethodChannel('opencv_ffi');
+
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+}
